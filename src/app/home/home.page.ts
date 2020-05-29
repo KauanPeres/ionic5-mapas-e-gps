271cdcbf702bd77ec8d5a6e2d875e75344dde2da
KauanPeres <kauanperesetec@gmail.com>
2020-05-29 20:24:59 -0300

    Automatizando a localização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { animation } from '@angular/animations';
+import { ILocal } from '../interfaces/ILocal';
 
 declare var google: any;
 
@@ -13,22 +14,40 @@ export class HomePage {
 
   map: any;
   posicaoAtual: any;
-  location1: any;
-  location2: any;
-  location3: any;
-  location4: any;
-  location5: any;
+
+public listaLocais: ILocal[] = [
+  {
+    lat: -22.476198,
+    lng: -48.557020,
+    titulo: 'Igreja'
+  },
+  {
+    lat: -22.476927,
+    lng: -48.558549,
+    titulo: 'ETC & TAL'
+  },
+  {
+    lat: -22.478446,
+    lng: -48.559370,
+    titulo: 'Rafa doces e salgados'
+  },
+  {
+    lat: -22.482287,
+    lng: -48.559084,
+    titulo: 'Edícula Capelozza'
+  },
+  {
+    lat: -22.483359,
+    lng: -48.556177,
+    titulo: 'Edícula da Bete'
+  }
+];
 
   @ViewChild('map', {read: ElementRef, static:false}) mapRef: ElementRef;
 
   constructor(private geolocation: Geolocation) {}
 
   public async showMap(){
-    const location1 = new google.maps.LatLng(-22.476198, -48.557020);   //Igreja
-    const location2 = new google.maps.LatLng(-22.476927, -48.558549);   //ETC & TAL
-    const location3 = new google.maps.LatLng(-22.478446, -48.559370);   //Rafa doces e salgados
-    const location4 = new google.maps.LatLng(-22.482287, -48.559084);   //Edícula Capelozza    
-    const location5 = new google.maps.LatLng(-22.483359, -48.556177);   //Edícula da Bete                      
 
     await this.buscaPosicao();
 
@@ -47,49 +66,23 @@ export class HomePage {
       animation: google.maps.Animation.DROP
     })
 
-    const marcadorPosicao1 = new google.maps.Marker({
-      position: location1,
-      map: this.map,
-      title: "Igreja",
-      //icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.DROP
-    })
-
-    const marcadorPosicao2 = new google.maps.Marker({
-      position: location2,
-      map: this.map,
-      title: "Rafa doces e salgados",
-      //icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.DROP
-    })
-
-    const marcadorPosicao3 = new google.maps.Marker({
-      position: location3,
-      map: this.map,
-      title: "ETC & TAL",
-      //icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.DROP
-    })
-
-    const marcadorPosicao4 = new google.maps.Marker({
-      position: location4,
-      map: this.map,
-      title: "Edícula Capelozza",
-      //icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.DROP
-    })
-
-    const marcadorPosicao5 = new google.maps.Marker({
-      position: location5,
-      map: this.map,
-      title: "Edícula da Bete ",
-      //icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.DROP
-    })
+    for(let local of this.listaLocais){
+      this.adicionarMarcador(local);
+    }
   }
 
   ionViewDidEnter(){
     this.showMap();
+  }
+
+  private adicionarMarcador(local:ILocal){
+    const { lat, lng, titulo } = local;
+
+    const marcador = new google.maps.Marker({
+      position: { lat, lng },
+      map: this.map,
+      title: titulo,
+    })
   }
 
   public async buscaPosicao(){
